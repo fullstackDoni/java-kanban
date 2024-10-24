7523a032e3ff8a4eb97979216289082e2f9c1c02
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2024-10-24 16:13:03 +0500

    "add new tasks"

--- src/Main.java
+++ src/Main.java
@@ -14,18 +14,28 @@ public class Main {
         Task task3 = new Task("Задача 3", "Описание задачи 3", Status.DONE);
         Task task4 = new Task("Задача 4", "Описание задачи 4", Status.DONE);
         Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
+        SubTask subtask1 = new SubTask(epic1.getId(), "Подзадача 1", "Описание подзадачи 1", Status.NEW);
         Epic epic2 = new Epic("Эпик 2", "Описание эпика 2");
         SubTask subTask2 = new SubTask(epic1.getId(), "Подзадача 1", "Описание подзадачи 1", Status.NEW);
 
         taskManager.addTask(task1);
         taskManager.addTask(task2);
+        taskManager.addTask(task3);
+        taskManager.addTask(task4);
         taskManager.addEpic(epic1);
+        taskManager.addSubtask(subtask1);
+        taskManager.addEpic(epic2);
         taskManager.addSubtask(subTask2);
 
         System.out.println("Получение задач:");
         printTask(taskManager, task1.getId());
         printTask(taskManager, task2.getId());
+        printTask(taskManager, task3.getId());
+        printTask(taskManager,task4.getId());
         printEpic(taskManager, epic1.getId());
+        printSubtask(taskManager, subtask1.getId());
+        printEpic(taskManager, epic2.getId());
+        printSubtask(taskManager,subTask2.getId());
 
         System.out.println("История просмотров:");
         printHistory(taskManager);
