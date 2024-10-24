14b14a706de6d20f40e7db5ece09c73f2cde85e8
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2024-10-24 17:08:29 +0500

    "add new epic"

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -9,65 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final Map<Integer, Node> taskMap = new HashMap<>();
-    private Node head;
-    private Node tail;
 
-    @Override
-    public void add(Task task) {
-        if (task != null) {
-            remove(task.getId());
-            linkLast(task);
-        }
-    }
-
-    @Override
-    public void remove(int id) {
-        Node node = taskMap.remove(id);
-        if (node != null) {
-            removeNode(node);
-        }
-    }
-
-    @Override
-    public List<Task> getHistory() {
-        List<Task> history = new LinkedList<>();
-        Node current = head;
-        while (current != null) {
-            history.add(current.getTask());
-            current = current.getNext();
-        }
-        return history;
-    }
-
-    private void linkLast(Task task) {
-        Node newNode = new Node(task, tail, null);
-        if (tail != null) {
-            tail.setNext(newNode);
-        }
-        tail = newNode;
-        if (head == null) {
-            head = newNode;
-        }
-        taskMap.put(task.getId(), newNode);
-    }
-
-    private void removeNode(Node node) {
-        Node prev = node.getPrev();
-        Node next = node.getNext();
-
-        if (prev != null) {
-            prev.setNext(next);
-        } else {
-            head = next;
-        }
-
-        if (next != null) {
-            next.setPrev(prev);
-        } else {
-            tail = prev;
-        }
-    }
 }
 
 
